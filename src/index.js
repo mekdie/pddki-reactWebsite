16ca349fd51fe0200890fb2e72c2767ec615951e
McDony Lee <mcdonyleee@gmail.com>
2022-11-14 03:57:10 +1100

    changed back to main

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import Main from './components/main';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>
 );
 
